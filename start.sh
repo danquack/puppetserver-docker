2f4076fe400ee2a2f5bd715d76d67f9e68409538
Dan Quackenbush <danquack@gmail.com>
2018-09-05 00:42:47 -0400

    Increased speed by removing n1
    Removed xargs numbered argument as puppet parser can handle multiple at once.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [ $1 == "validate" ]; then
-	cd manifests/ && find -name '*.pp' | xargs -n 1 -t puppet parser validate
+	cd manifests/ && find -name '*.pp' | xargs puppet parser validate
 elif [ $1 == "lint" ]; then
  	cd manifests/ && find -name '*.pp' | xargs -n 1 -t puppet-lint $OPTIONS
 elif [ $1 == "version" ]; then
